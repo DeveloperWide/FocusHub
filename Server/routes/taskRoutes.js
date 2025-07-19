fbdb0d3a37b1414158ab14f1ef6621ee4b8c5c32
Mahesh rana <maheshisdelta@gmail.com>
2025-07-19 16:38:56 +0530

    Updated Code

--- Server/routes/taskRoutes.js
+++ Server/routes/taskRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const Task = require("../models/Task")
 const router = express.Router();
 
+// Get all Tasks
 router.get("/", async (req, res) => {
     try {
         const allTasks = await Task.find();
@@ -18,12 +19,24 @@ router.get("/", async (req, res) => {
     }
 });
 
+// Create Task
 router.post("/", async (req, res) => {
     try{
-        console.log(req.body)
+        const newTask = new Task(req.body);
+        const svdTask = await newTask.save();
     }catch(err){
         console.log(err);
     }
+});
+
+// Get Task
+router.get("/:id", async (req, res) => {
+    let task = await Task.findById(req.params.id);
+    res.json({
+        success: true,
+        message: "Your Task",
+        data: task
+    })
 })
 
 module.exports = router;
