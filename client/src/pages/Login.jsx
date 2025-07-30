c39dc27d49f2bfaceb215e1b15116ab214fffbd2
Mahesh rana <maheshisdelta@gmail.com>
2025-07-31 05:01:46 +0530

    Updated Code

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
     <div>
         <div>
-            <input type="email" name="email" id="email" className='border mt-7 my-2 w-full px-3 py-2 outline-none rounded-2xl border-gray-400' placeholder='Email Address'/>
-            <input type="password" name="email" id="email" className='border my-2 mb-0 w-full px-3 py-2 outline-none rounded-2xl border-gray-400' placeholder='Password'/>
-            <p className='text-blue-500 capitalize ms-2 my-1 cursor-pointer`'>Forgot password?</p>
-            <button className='w-full my-3 px-3 py-2 bg-blue-500 rounded-2xl font-bold text-white'>Login</button>
-            <p className='text-center text-black mt-4'>Not a member? <Link to={"/signup"} className='text-blue-500 cursor-pointer'>Signup now</Link></p>
+            <input type="email" name="email" id="email" className='border mt-4 w-full px-3 py-2 outline-none rounded-2xl border-gray-400' placeholder='Email Address'/>
+            <input type="password" name="email" id="email" className='border my-1 mb-0 w-full px-3 py-2 outline-none rounded-2xl border-gray-400' placeholder='Password'/>
+            <p className='text-blue-500 capitalize ms-2 my-0.5 cursor-pointer`'>Forgot password?</p>
+            <button className='w-full my-2 px-3 py-2 cursor-pointer bg-gradient-to-r from-blue-600 to-blue-800 rounded-2xl font-semibold text-xl text-white'>Login</button>
+            <p className='text-center text-black mt-2'>Not a member? <Link to={"/signup"} className='text-blue-500 cursor-pointer'>Signup now</Link></p>
         </div>
     </div>
   )
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import { Link } from "react-router-dom"
+
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <div className='flex flex-col'>
+      <input type="email" name="email" id="email" placeholder='Email Address' className='mt-3'/>
+      <input type="password" name="password" id="password" placeholder='Password'/>
+      <input type="password" name="confirmPassword" id="confirmPassword" placeholder='Confirm Password'/>
+      <button className='w-full h-12 text-xl rounded-xl font-semibold text-white bg-gradient-to-r cursor-pointer from-blue-600 to-blue-800'>Signup</button>
+      <p className="text-center text-black mt-2">Already Have a Account ? <Link to="/login" className="text-blue-500 cursor-pointer">Login here</Link></p>
+    </div>
   )
 }
 
