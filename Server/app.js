b414aba1d6368ae2860c7b0850ac58cab50cfe56
Mahesh rana <maheshisdelta@gmail.com>
2025-08-01 23:41:58 +0530

    Updated Code in Server

--- Server/app.js
+++ Server/app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const cookieParser = require("cookie-parser")
+// const cookieParser = require("cookie-parser")
 const { connectDb } = require("./config/db");
+const dotenv = require("dotenv").config();
 const PORT = 8080;
 
 // Connect to Db
@@ -16,18 +17,18 @@ connectDb().then(() => {
 // Routes
 const taskRoutes = require("./routes/taskRoutes");
 const goalRoutes = require("./routes/goalRoutes");
-// const authRoutes = require("./routes/authRoutes")
+const authRoutes = require("./routes/authRoutes")
 
 
 app.use(express.json());
-app.use(cookieParser())
+// app.use(cookieParser())
 app.use(cors({
     origin: "http://localhost:5173/"
 }))
 
 app.use("/api/tasks" , taskRoutes);
 app.use("/api/goals" , goalRoutes)
-// app.use("/api/auth" , authRoutes)
+app.use("/api/auth" , authRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server is listing on PORT 8080`)
