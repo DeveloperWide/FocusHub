f6aabb96a38dc2fa3727ea1621f22bd25ad1c3d8
Mahesh rana <maheshisdelta@gmail.com>
2025-08-15 21:02:03 +0530

    Updated Code

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 
+// Connect to MongoDB
 module.exports.connectDb = async () => {
-    await mongoose.connect("mongodb://127.0.0.1:27017/focushub")
+    await mongoose.connect(process.env.DB_URL)
 }
