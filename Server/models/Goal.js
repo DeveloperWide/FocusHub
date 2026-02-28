a009ef61ad00bf706d7e3c1fb46317f02293986f
DeveloperWide <maheshisdelta@gmail.com>
2026-03-01 05:19:08 +0530

    Updated: model & controller code for tag in our goal

--- Server/models/Goal.js
+++ Server/models/Goal.js
@@ -1,12 +1,17 @@
 let mongoose = require("mongoose");
-const {Schema , model} = mongoose;
+const { Schema, model } = mongoose;
 
-const goalSchema = new Schema({
+const goalSchema = new Schema(
+  {
     title: {
-        type: String,
-        required: true
-    }
-});
+      type: String,
+      required: true,
+    },
+
+    tag: { type: String, required: true },
+  },
+  { timestamps: true },
+);
 
 const Goal = model("Goal", goalSchema);
 module.exports = Goal;
