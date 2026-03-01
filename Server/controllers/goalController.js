445616a17dac04b811f255647e6278c0d5fae4c4
Mahesh Rana <152925826+DeveloperWide@users.noreply.github.com>
2026-03-01 19:26:40 +0530

    Fix/server goal (#10)
    * Updated: model & controller code for tag in our goal

--- Server/controllers/goalController.js
+++ Server/controllers/goalController.js
@@ -3,41 +3,51 @@ const wrapAsync = require("../utils/asyncWrapper");
 const ExpressError = require("../utils/ExpressError");
 
 module.exports.getGoals = wrapAsync(async (req, res, next) => {
-    let allGoals = await Goal.find();
+  let allGoals = await Goal.find();
 
-    res.status(200).json({
-        success: true,
-        message: "All Your Goals Here",
-        data: allGoals
-    })
-})
+  res.status(200).json({
+    success: true,
+    message: "All Your Goals Here",
+    data: allGoals,
+  });
+});
 
-module.exports.createGoal = wrapAsync(
-    async (req, res, next) => {
-        const allGoals = await Goal.find();
-        if(allGoals.length === 3){
-            throw new ExpressError(406, "Only 3 Goals Are Accepted")
-        }
+module.exports.createGoal = wrapAsync(async (req, res, next) => {
+  const { title, tag } = req.body;
+  const allGoals = await Goal.find();
+  if (allGoals.length === 3) {
+    throw new ExpressError(406, "Only 3 Goals Are Accepted");
+  }
 
-        const newGoal = new Goal(req.body);
-        const svdGoal = await newGoal.save();
-        if(!svdGoal) throw new ExpressError(500, "Failed To Create Goal")
-        console.log(svdGoal);
-        res.status(200).send({
-            success: true,
-            message: "Your Goal Saved",
-            data: req.body
-        })
-    }
-)
+  if (!title || !tag) {
+    return res.status(400).json({
+      message: "All fields are Required",
+    });
+  }
 
-module.exports.deleteGoal = wrapAsync(
-    async (req, res, next) => {
-       const goalToBeDeleted = await Goal.findByIdAndDelete(req.params.id);
-       if(!goalToBeDeleted) throw new ExpressError(404, "Goal Not Found")
-        res.status(200).json({
-            success: true,
-            message: "Goal Deleted Successfully"
-        })
-    }
-)
+  const newGoal = new Goal({
+    title,
+    tag,
+  });
+
+  const svdGoal = await newGoal.save();
+
+  if (!svdGoal) throw new ExpressError(500, "Failed To Create Goal");
+
+  res.status(200).send({
+    success: true,
+    message: "Your Goal Saved",
+    data: svdGoal,
+  });
+});
+
+module.exports.deleteGoal = wrapAsync(async (req, res, next) => {
+  const goalToBeDeleted = await Goal.findByIdAndDelete(req.params.id);
+
+  if (!goalToBeDeleted) throw new ExpressError(404, "Goal Not Found");
+
+  res.status(200).json({
+    success: true,
+    message: "Goal Deleted Successfully",
+  });
+});
--- Server/routes/goalRoutes.js
+++ Server/routes/goalRoutes.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const goalController = require("../controllers/goalController")
+const goalController = require("../controllers/goalController");
 
 // Get All Goals
 router.get("/", goalController.getGoals);
 
 // Create New Goal
-router.post("/", goalController.createGoal)
+router.post("/", goalController.createGoal);
 
 // Delete Goal
-router.delete("/:id", goalController.deleteGoal)
+router.delete("/:id", goalController.deleteGoal);
 
 module.exports = router;
