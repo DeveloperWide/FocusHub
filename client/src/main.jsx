a1932624e7b25bcd47ec9ff0e9672fce9292056d
DeveloperWide <maheshisdelta@gmail.com>
2026-03-12 02:35:37 +0530

    fix: import in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { store } from "./app/store.js";
-import { Analytics } from "@vercel/analytics";
+import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")).render(
