3fd2e23f582020d45f8a050b229e6c7d80726a24
Mahesh Rana <152925826+DeveloperWide@users.noreply.github.com>
2026-03-12 02:01:34 +0530

    feat: add vercel analytics (#35)

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,11 +5,12 @@ import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { store } from "./app/store.js";
-
+import { Analytics } from "@vercel/analytics";
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
+    <Analytics />
   </BrowserRouter>,
 );
